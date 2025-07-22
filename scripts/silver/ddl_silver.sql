948dec593854b3f3953e932db9b2f15a16805afc
Rohan-Basnet <rohbas05@gmail.com>
2025-07-22 21:32:06 +0545

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -24,7 +24,7 @@ CREATE TABLE silver.crm_cust_info(
     cst_marital_status VARCHAR(50),
     cst_gndr VARCHAR(50),
     cst_create_date DATE,
-	dwh_create_date DATETIME2 DEFAULT GETDATE()
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
 IF OBJECT_ID('silver.crm_prd_info','U') IS NOT NULL
@@ -37,8 +37,7 @@ CREATE TABLE silver.crm_prd_info(
     prd_line VARCHAR(50),
     prd_start_dt DATE,
     prd_end_dt DATE,
-	dwh_create_date DATETIME2 DEFAULT GETDATE()
-
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
 IF OBJECT_ID('silver.crm_sales_details','U') IS NOT NULL
@@ -53,8 +52,7 @@ CREATE TABLE silver.crm_sales_details(
     sls_sales INT,
     sls_quantity INT,
     sls_price INT,
-	dwh_create_date DATETIME2 DEFAULT GETDATE()
-
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
 IF OBJECT_ID('silver.erp_cust_az12','U') IS NOT NULL
@@ -64,7 +62,6 @@ CREATE TABLE silver.erp_cust_az12(
     bdate VARCHAR(50),
     gen VARCHAR(50),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
-
 );
 
 IF OBJECT_ID('silver.erp_loc_a101','U') IS NOT NULL
@@ -72,8 +69,7 @@ IF OBJECT_ID('silver.erp_loc_a101','U') IS NOT NULL
 CREATE TABLE silver.erp_loc_a101(
     cid VARCHAR(50),
     cntry VARCHAR(50),
-	dwh_create_date DATETIME2 DEFAULT GETDATE()
-
+     dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
 IF OBJECT_ID('silver.erp_px_cat_g1v2','U') IS NOT NULL
@@ -83,8 +79,7 @@ CREATE TABLE silver.erp_px_cat_g1v2(
     cat VARCHAR(50),
     subcat VARCHAR(50),
     maintenance VARCHAR(50),
-	dwh_create_date DATETIME2 DEFAULT GETDATE()
-
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
 
