8402d505e03e403c760482a86a6e22901f3ccd1b
Rohan-Basnet <rohbas05@gmail.com>
2026-01-10 09:35:03 +0545

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -9,6 +9,9 @@ Run this script to re-define the DDL structure of 'silver' tables.
 ==============================================================
 */
 
+-- =============================================================================
+-- Create table: silver.crm_cust_info
+-- =============================================================================
 IF OBJECT_ID('silver.crm_cust_info','U') IS NOT NULL
     DROP TABLE silver.crm_cust_info;
 CREATE TABLE silver.crm_cust_info(
@@ -22,6 +25,9 @@ CREATE TABLE silver.crm_cust_info(
     dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
+-- =============================================================================
+-- Create table: silver.crm_prd_info
+-- =============================================================================
 IF OBJECT_ID('silver.crm_prd_info','U') IS NOT NULL
     DROP TABLE silver.crm_prd_info;
 CREATE TABLE silver.crm_prd_info(
@@ -35,6 +41,9 @@ CREATE TABLE silver.crm_prd_info(
     dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
+-- =============================================================================
+-- Create table: silver.crm_sales_details
+-- =============================================================================
 IF OBJECT_ID('silver.crm_sales_details','U') IS NOT NULL
     DROP TABLE silver.crm_sales_details;
 CREATE TABLE silver.crm_sales_details(
@@ -50,6 +59,9 @@ CREATE TABLE silver.crm_sales_details(
     dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
+-- =============================================================================
+-- Create table: silver.erp_cust_az12
+-- =============================================================================
 IF OBJECT_ID('silver.erp_cust_az12','U') IS NOT NULL
     DROP TABLE silver.erp_cust_az12;
 CREATE TABLE silver.erp_cust_az12(
@@ -59,6 +71,9 @@ CREATE TABLE silver.erp_cust_az12(
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
+-- =============================================================================
+-- Create table: silver.erp_loc_a101
+-- =============================================================================
 IF OBJECT_ID('silver.erp_loc_a101','U') IS NOT NULL
     DROP TABLE silver.erp_loc_a101;
 CREATE TABLE silver.erp_loc_a101(
@@ -67,6 +82,9 @@ CREATE TABLE silver.erp_loc_a101(
      dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
+-- =============================================================================
+-- Create table: silver.erp_px_cat_g1v2
+-- =============================================================================
 IF OBJECT_ID('silver.erp_px_cat_g1v2','U') IS NOT NULL
     DROP TABLE silver.erp_px_cat_g1v2;
 CREATE TABLE silver.erp_px_cat_g1v2(
